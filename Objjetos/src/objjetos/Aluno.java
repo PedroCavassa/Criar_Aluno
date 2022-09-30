52a54c1896a25c56f5aa5c6b488c7fc65a4eacf4
PEDRO HENRIQUE CAVASSA MULATTI NUNES <pedro_hc_nunes@estudante.sesisenai.org.br>
2022-09-30 12:31:45 -0300

    aa

---
+++ Objjetos/src/objjetos/Aluno.java
@@ -0,0 +1,19 @@
+package objjetos;
+
+public class Aluno extends Pessoa{
+    private String matricula;
+
+    public Aluno(String nome, int idade, String cpf, String matricula){
+        super(nome, idade, cpf);
+        this.matricula = matricula;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    
+}
